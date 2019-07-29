d9aecb6f68fdd4d1b83789b7bab755065781176e
csn <644753958@qq.com>
2019-07-29 22:14:59 +0800

    126. Word Ladder II

---
+++ src/typical/bfsdfs/WordLadderII.java
@@ -0,0 +1,183 @@
+package typical.bfsdfs;
+
+import java.util.*;
+
+/**
+ * Description:
+ * 126. Word Ladder II
+ * Given two words (beginWord and endWord), and a dictionary's word list, find all shortest transformation sequence(s) from beginWord to endWord, such that:
+ *
+ * Only one letter can be changed at a time
+ * Each transformed word must exist in the word list. Note that beginWord is not a transformed word.
+ * Note:
+ *
+ * Return an empty list if there is no such transformation sequence.
+ * All words have the same length.
+ * All words contain only lowercase alphabetic characters.
+ * You may assume no duplicates in the word list.
+ * You may assume beginWord and endWord are non-empty and are not the same.
+ * Example 1:
+ *
+ * Input:
+ * beginWord = "hit",
+ * endWord = "cog",
+ * wordList = ["hot","dot","dog","lot","log","cog"]
+ *
+ * Output:
+ * [
+ *   ["hit","hot","dot","dog","cog"],
+ *   ["hit","hot","lot","log","cog"]
+ * ]
+ *
+ * @author csn
+ */
+public class WordLadderII {
+    /*
+        1.bfs + dfs
+     */
+    public List<List<String>> findLadders1(String start, String end, List<String> wordList) {
+        HashSet<String> dict = new HashSet<>(wordList);
+        List<List<String>> res = new ArrayList<>();
+        HashMap<String, ArrayList<String>> nodeNeighbors = new HashMap<>();// Neighbors for every node
+        HashMap<String, Integer> distance = new HashMap<>();// Distance of every node from the start node
+        ArrayList<String> solution = new ArrayList<>();
+
+        dict.add(start);
+        bfs(start, end, dict, nodeNeighbors, distance);
+        dfs(start, end, dict, nodeNeighbors, distance, solution, res);
+        return res;
+    }
+
+    // BFS: Trace every node's distance from the start node (level by level).
+    private void bfs(String start, String end, Set<String> dict, HashMap<String, ArrayList<String>> nodeNeighbors, HashMap<String, Integer> distance) {
+        for (String str : dict)
+            nodeNeighbors.put(str, new ArrayList<>());
+
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(start);
+        distance.put(start, 0);
+
+        while (!queue.isEmpty()) {
+            int count = queue.size();
+            boolean foundEnd = false;
+            for (int i = 0; i < count; i++) {
+                String cur = queue.poll();
+                int curDistance = distance.get(cur);
+                ArrayList<String> neighbors = getNeighbors(cur, dict);
+
+                for (String neighbor : neighbors) {
+                    nodeNeighbors.get(cur).add(neighbor);
+                    if (!distance.containsKey(neighbor)) {// Check if visited
+                        distance.put(neighbor, curDistance + 1);
+                        if (end.equals(neighbor))// Found the shortest path
+                            foundEnd = true;
+                        else
+                            queue.offer(neighbor);
+                    }
+                }
+            }
+
+            if (foundEnd)
+                break;
+        }
+    }
+
+    // Find all next level nodes.
+    private ArrayList<String> getNeighbors(String node, Set<String> dict) {
+        ArrayList<String> res = new ArrayList<>();
+        char[] chs = node.toCharArray();
+
+        for (char ch ='a'; ch <= 'z'; ch++) {
+            for (int i = 0; i < chs.length; i++) {
+                if (chs[i] == ch) continue;
+                char old_ch = chs[i];
+                chs[i] = ch;
+                if (dict.contains(String.valueOf(chs))) {
+                    res.add(String.valueOf(chs));
+                }
+                chs[i] = old_ch;
+            }
+
+        }
+        return res;
+    }
+
+    // DFS: output all paths with the shortest distance.
+    private void dfs(String cur, String end, Set<String> dict, HashMap<String, ArrayList<String>> nodeNeighbors, HashMap<String, Integer> distance, ArrayList<String> solution, List<List<String>> res) {
+        solution.add(cur);
+        if (end.equals(cur)) {
+            res.add(new ArrayList<>(solution));
+        } else {
+            for (String next : nodeNeighbors.get(cur)) {
+                if (distance.get(next) == distance.get(cur) + 1) {
+                    dfs(next, end, dict, nodeNeighbors, distance, solution, res);
+                }
+            }
+        }
+        solution.remove(solution.size() - 1);
+    }
+
+    /*
+          2.Basic idea is using a HashMap to record all route ending at the key and checked whether the key equals EndWord.
+            Do remember to remove the checked words from dictionary.
+     */
+    public List<List<String>> findLadders2(String beginWord, String endWord, List<String> wordList) {
+        List<List<String>> res = new ArrayList<>();
+        Set<String> words = new HashSet<>(wordList);
+
+        Set<String> start = new HashSet<>();
+        start.add(beginWord);
+        boolean found = false;
+
+        // use hashMap to store all possible route ending at key
+        Map<String, List<List<String>>> map = new HashMap<>();
+        List<String> init = new ArrayList<>();
+        init.add(beginWord);
+        map.put(beginWord, new ArrayList<>());
+        map.get(beginWord).add(init);
+
+
+        while(!words.isEmpty() && !found && !start.isEmpty()){
+            // eliminate all previous layer words from dict
+            words.removeAll(start);
+            // use another set to record next layers' words
+            Set<String> newStart = new HashSet<>();
+
+            // iterate through all new starts
+            for(String s: start){
+                char[] chs = s.toCharArray();
+                List<List<String>> endPath  = map.get(s);
+                for(int i = 0; i < chs.length; i++){
+                    // randomly change a character
+                    for(char ch='a';ch<='z';ch++){
+                        if(chs[i]==ch) continue;
+                        char tmp = chs[i];
+                        chs[i] = ch;
+                        String word = new String(chs);
+                        //check if it is in the dict, if so new start found, extending all routes
+                        if(words.contains(word)){
+                            newStart.add(word);
+                            for(List<String> path : endPath){
+                                List<String> nextPath = new ArrayList<>(path);
+                                nextPath.add(word);
+                                map.putIfAbsent(word, new ArrayList<>());
+                                map.get(word).add(nextPath);
+                                if(word.equals(endWord)){
+                                    found = true;
+                                    res.add(nextPath);
+                                }
+                            }
+
+                        }
+                        chs[i] = tmp;
+                    }
+                }
+                map.remove(s);
+            }
+            // clear the previous layers words and update
+            start.clear();
+            start.addAll(newStart);
+        }
+        return res;
+    }
+}
